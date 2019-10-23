035be5388f4728a6ff487da4c4f882332a58cf3f
erikkowalski <erikkowalski@mac.com>
2019-10-23 01:00:24 -0400

    inital commit

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+
+<div class="col-sm-3">
+
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package demo
+ */
+
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
+</div>
